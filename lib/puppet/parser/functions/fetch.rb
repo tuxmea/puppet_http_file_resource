47bef13883d6dc86f029d845b4c881e400b969ba
Martin Alfke <martin.alfke@buero20.org>
2011-07-27 09:18:35 +0200

    add functionality to function

--- lib/puppet/parser/functions/fetch.rb
+++ lib/puppet/parser/functions/fetch.rb
@@ -1,6 +1,9 @@
+require 'net/http'
 module Puppet::Parser::Functions
 	newfunction(:fetch) do |args|
-		uri = args[0]
+		url = URI.parse($args)
+		req = Net::HTTP::Get.new(url.path)
+		res = Net::HTTP.start(url.host, url.port) {|http| http.request(req) }
 	end
 end
 
