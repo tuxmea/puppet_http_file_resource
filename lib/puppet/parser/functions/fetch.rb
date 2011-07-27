92c56bd6a6e9baf51c78727868f2baeb51e0dc6b
Martin Alfke <martin.alfke@buero20.org>
2011-07-27 11:20:54 +0200

    some corrections on fetch function

--- lib/puppet/parser/functions/fetch.rb
+++ lib/puppet/parser/functions/fetch.rb
@@ -1,9 +1,11 @@
-require 'net/http'
-module Puppet::Parser::Functions
-	newfunction(:fetch) do |args|
-		url = URI.parse($args)
-		req = Net::HTTP::Get.new(url.path)
-		res = Net::HTTP.start(url.host, url.port) {|http| http.request(req) }
-	end
+Puppet::Parser::Functions::newfunction(:fetch, :type => :rvalue) do |args|
+	require 'net/http'
+	require 'uri'
+	url = URI.parse(args[0])
+	req = Net::HTTP::Get.new(url.path)
+	res = Net::HTTP.start(url.host, url.port) {|http|
+	  http.request(req) 
+	}
+	return res.body
 end
 
